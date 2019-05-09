a70e7529e4aa152f3259c95163729db6bb7a46c8
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-05-09 13:16:42 -0600

    i'm so sorry

---
+++ data/UserSample.js
@@ -0,0 +1,13 @@
+const UserSample = 
+  {
+    "id": 1,
+    "name": "Nyasia Weber",
+    "address": "270 August Meadows, Maribelside SD 36129",
+    "email": "Jordane_Schultz@yahoo.com",
+    "strideLength": 4.7,
+    "dailyStepGoal": 8000
+  }
+
+  if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
+  module.exports = UserSample;
+}
