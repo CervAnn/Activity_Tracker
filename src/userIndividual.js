ff171d3dda8880af1d2e3671a63f6aac24ed2a06
Ann Cerveny <cervann11@gmail.com>
2019-05-11 11:55:32 -0600

    Create getAverageFluids method that passes tests and calcs avg fl ozs per day

--- src/userIndividual.js
+++ src/userIndividual.js
@@ -1,29 +1,41 @@
 class User {
   constructor(userData) {
-    this.userData = userData[0]
+    this.userData = userData;
     // this.userData = userData[Math.floor(Math.random()*userData.length)]
   }
 
-  returnFirstName() {
-   let userName = this.userData.name.split(" ")
+  returnFirstName(id) {
+    let userId = id - 1;
+   let userName = this.userData[id - 1].name.split(" ");
    return userName[0];
   }
 
-  compareStepGoal(data) {
+  findUserData(data, id) {
+    var correctUser = data.find(user => user.id === id);
+    return correctUser;
+  } 
+
+  compareStepGoal(data, id) {
     var goals = data.map(user => {
       return user.dailyStepGoal;
     });
     var averageStepGoal = goals.reduce((acc, goal) => {
       return Math.floor((acc += goal) / 5);
     }, 0);
-    if (this.userData.dailyStepGoal > averageStepGoal) {
-      return `Your daily step goal is ${this.userData.dailyStepGoal - averageStepGoal} more than the average user.`;
+    if (this.userData[id - 1].dailyStepGoal > averageStepGoal) {
+      return `Your daily step goal is ${this.userData[id - 1].dailyStepGoal - averageStepGoal} more than the average user.`;
     } else {
-      return `Your daily step goal is ${averageStepGoal - this.userData.dailyStepGoal} less than the average user.`;
+      return `Your daily step goal is ${averageStepGoal - this.userData[id - 1].dailyStepGoal} less than the average user.`;
     }
   }
-  getAverageFluids() {
-    console.log('feafe', this.userData.id)
+  getAverageFluids(data, id) {
+    const numOunces = data[id - 1].hydrationData.map(object => {
+      return object.numOunces;
+    })
+    const averageOunces = numOunces.reduce((acc, oz) => {
+      return acc += oz;
+    }, 0)
+    return Math.floor(averageOunces / numOunces.length);
   }
 }
 
--- test/userIndividual-test.js
+++ test/userIndividual-test.js
@@ -18,17 +18,22 @@ describe('User', function() {
 
   it('should return first name of the name property of mockUserData', function() {
     const user = new User(longUserData);
-    user.returnFirstName();
-    expect(user.returnFirstName()).to.equal('Nyasia');
+    user.returnFirstName(1);
+    expect(user.returnFirstName(1)).to.equal('Nyasia');
   });
+
+  it('should return User data based on User ID', function() {
+    const user = new User(longUserData);
+  expect(user.findUserData(longUserData, 1)).to.equal(longUserData[0]);
+  })
 
   it('should compare individual step goal to all user step goals', function() {
     const user = new User(longUserData);
-    expect(user.compareStepGoal(longUserData)).to.equal(`Your daily step goal is 6035 more than the average user.`)
+    expect(user.compareStepGoal(longUserData, 1)).to.equal(`Your daily step goal is 6035 more than the average user.`)
   });
 
   it('should calculate the average amount of fluids they consume per day', function() {
     const user = new User(longUserData);
-    expect(user.getAverageFluids().to.equal(64))
+    expect(user.getAverageFluids(hydrationData, 1)).to.equal(55)
   })
 });
