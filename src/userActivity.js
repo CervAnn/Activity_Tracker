b9d014df193e3678a8c89100a8a154ce1413f0a8
Ann Cerveny <cervann11@gmail.com>
2019-05-15 20:26:32 -0600

    Add complete method 2 functionality and testing
    Co-authored-by: Jacqueline Moore <jacquelinebmoore@gmail.com>
    Co-authored-by: Ann Cerveny <CervAnn11@gmail.com>

--- src/userActivity.js
+++ src/userActivity.js
@@ -2,10 +2,6 @@ class userActivity {
   constructor(userData, id) {
         this.userData = userData[id-1];
     }
-    
-// For a specific day (specified by a date), return the miles a user has walked 
-// based on their number of steps (use their strideLength to help calculate this)
-
 
     walkedMilesUser(dataForActivity, dataForUser, id, date) {
         let dateOfSteps = dataForActivity[id - 1].activityData
@@ -15,6 +11,11 @@ class userActivity {
         return parseFloat(totalMiles.toFixed(2))
     }
 
+    activeMinutesUser(dataForActivity, id, date) {
+        let dateOfMinutesActive = dataForActivity[id - 1].activityData
+        .find(obj => parseInt(obj.date.split('/')) === parseInt(date.split('/')));
+        return dateOfMinutesActive.minutesActive
+    }
 }
 
 if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
--- test/userActivity-test.js
+++ test/userActivity-test.js
@@ -11,13 +11,14 @@ describe('userActivity', function() {
     expect(userActivity).to.be.a('function');
   });
 
-// For a specific day (specified by a date), return the miles a user has walked 
-// based on their number of steps (use their strideLength to help calculate this)
-
   it('should return miles walks based on steps for a given day', function() {
     const userTrek = new userActivity(userData, 1);
     expect(userTrek.walkedMilesUser(activityData, userData, 1, "06/05/2019")).to.deep.equal(6.56)
   })
 
+  it('should return minutes active for a user on a given date', function() {
+    const userTrek = new userActivity(userData, 1);
+    expect(userTrek.activeMinutesUser(activityData, 1, "15/05/2019")).to.deep.equal(228)
+  })
 
 });
